2cf7b9a552f77c3c266806170dbba1ceddfcc1ca
Brian Voter <bvoter97@gmail.com>
2023-05-29 17:40:18 -0400

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import DogList from './DogList';
+import DogDetails from './DogDetails';
+import Nav from './Nav';
+import axios from 'axios';
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+const DOGS_API_URL = "http://localhost:5001/dogs";
 
 function App() {
+  const [dogs, setDogs] = useState(null);
+
+  async function loadDogs() {
+    const result = await axios.get(DOGS_API_URL);
+
+    const dogs = result.data.map(dog => ({...dog, id: uuid()}));
+
+    setDogs(dogs);
+  }
+
+  if (!dogs) {
+    loadDogs();
+
+    return (
+      <p>loading dogs...</p>
+    );
+  }
+
   return (
-    <div className="App">
-      <h1>Good luck!</h1>
-    </div>
+    <BrowserRouter>
+      <Nav dogs={dogs}/>
+      <Routes>
+        <Route path="/dogs" element={<DogList />} />
+        <Route path="/dogs/:name" element={<DogDetails />} />
+        <Route path="*" element={<Navigate to="/dogs" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./nav.css"
+
+function Nav({ dogs }) {
+  return <nav>
+    {dogs?.map(dog => <Link key={dog.id} className="nav-link">{dog.name}</Link>)}
+  </nav>;
+}
+
+export default Nav;
